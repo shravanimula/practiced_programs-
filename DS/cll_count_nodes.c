e1e89bfae60f99def4a814b8dbf90f2809070e71
shravani <shravani.mula@thundersoft.com>
2022-07-11 20:12:36 +0530

    count_nodes

---
+++ DS/cll_count_nodes.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct st{
+	int data;
+	struct st *link;
+};
+struct st *add(struct st *);
+void display(struct st *);
+void count_nodes(struct st *);
+int main()
+{
+	struct st *head=NULL;
+	int choice;
+	while(1)
+	{
+		printf("1.add\t2.display\t3.count_nodes\t4.exit\n");
+		printf("enter ur choice:\n");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:head=add(head);
+			       break;
+			case 2:display(head);
+			       break;
+			case 3:count_nodes(head);
+			       break;
+			case 4:exit(0);
+		}
+	}
+}
+struct st *add(struct st *ptr)
+{
+	struct st *newnode=NULL,*temp=NULL;
+	newnode=malloc(sizeof(struct st));
+	if(newnode==NULL)
+	{
+		printf("memory is not allocated\n");
+	}
+	else
+	{
+		printf("enter a data of st:\n");
+		scanf("%d",&newnode->data);
+		newnode->link=NULL;
+		if(ptr==NULL)
+		{
+			ptr=newnode;
+			//ptr->link=newnode;
+		}
+		else
+		{
+			temp=ptr;
+			while(temp->link!=ptr)
+			{
+				temp=temp->link;
+			}
+			temp->link=newnode;
+		}
+		newnode->link=ptr;
+	}
+	return ptr;
+}
+void display(struct st *ptr)
+{
+	struct st *temp=NULL;
+	if(ptr==NULL)
+	{
+		printf("list is empty\n");
+	}
+	else
+	{
+		temp=ptr;
+		do{
+			printf("%d\n",temp->data);
+			temp=temp->link;
+		}while(temp!=ptr);
+	//	printf("%d\n",temp->data);
+	}
+}
+void count_nodes(struct st *ptr)
+{
+	struct st *temp=NULL;
+	int cnt=0;
+	if(ptr==NULL)
+	{
+		printf("list is empty\n");
+	}
+	else
+	{
+		temp=ptr;
+		do{
+			cnt++;
+			temp=temp->link;
+		}while(temp!=ptr);
+	}
+	printf("count the nodesin cll =%d\n",cnt);
+}
